f8c788756970104d056872433c58cbf0ac3d28a0
Mateus Silva <trabalhomateusjs521@gmail.com>
2023-07-01 09:30:47 -0300

    Uploading the project

---
+++ missionarios e canibais/canibais.py
@@ -0,0 +1,244 @@
+#Somente modifiquei o em profundidade, por algum motivo ele não soluciona corretamente
+
+class BuscaProfundidade:
+    def __init__(self, start_state, goal_state):
+        self.estados_visitados = set()
+        self.start_state = start_state
+        self.goal_state = goal_state
+
+    def is_valid_state(self, state):
+        missionarios_esquerda, canibais_esquerda, barco, missionarios_direita, canibais_direita = state
+
+        # Verifica se o estado é válido
+        if missionarios_esquerda < 0 or canibais_esquerda < 0 or missionarios_direita < 0 or canibais_direita < 0:
+            return False
+        if missionarios_esquerda > 3 or canibais_esquerda > 3 or missionarios_direita > 3 or canibais_direita > 3:
+            return False
+        if (missionarios_esquerda < canibais_esquerda and missionarios_esquerda > 0) or (missionarios_direita < canibais_direita and missionarios_direita > 0):
+            print("ERRO: Há mais canibais que missionários em uma das bordas!\n")
+            return False
+        print(">>>Estado aceito.<<<\n")
+        return True
+
+    def generate_next_states(self, state):
+        states = []
+        missionarios_esquerda, canibais_esquerda, barco, missionarios_direita, canibais_direita = state
+
+        if barco == 'esquerda':
+            for m in range(3):
+                for c in range(3):
+                    if m + c > 0 and m + c <= 2:
+                        new_state = (
+                            missionarios_esquerda - m,
+                            canibais_esquerda - c,
+                            'direita',
+                            missionarios_direita + m,
+                            canibais_direita + c
+                        )
+                        print(f"Novo estado: {new_state}")
+                        if self.is_valid_state(new_state):
+                            states.append(new_state)
+        else:
+            for m in range(3):
+                for c in range(3):
+                    if m + c > 0 and m + c <= 2:
+                        new_state = (
+                            missionarios_esquerda + m,
+                            canibais_esquerda + c,
+                            'esquerda',
+                            missionarios_direita - m,
+                            canibais_direita - c
+                        )
+                        print(f"Novo estado: {new_state}")
+                        if self.is_valid_state(new_state):
+                            states.append(new_state)
+
+        return states
+
+    def dfs(self, state, path, depth):
+        self.estados_visitados.add(state)
+        print(f"\n---------Estado atual: {state} (Profundidade: {depth})------------")
+        if state == self.goal_state:
+            print(f"Profundidade da solução: {len(path) - 1}")
+            return path
+
+        next_states = self.generate_next_states(state)
+        for next_state in next_states:
+            print(f"\nVerificando próximo estado: {next_state}")
+            if next_state not in self.estados_visitados:
+                print("Estado não visitado!!")
+                new_path = path + [next_state]
+                result = self.dfs(next_state, new_path, depth + 1)  # Incrementa a profundidade
+                if result:
+                    return result
+            else:
+                print("Estado já visitado!!")
+        return None
+
+    def resolver(self):
+        return self.dfs(self.start_state, [self.start_state], 0)  # Inicia a profundidade com 0
+
+
+class BuscaGulosa:
+    def __init__(self, start_state, goal_state):
+        self.estados_visitados = set()
+        self.start_state = start_state
+        self.goal_state = goal_state
+
+    def is_valid_state(self, state):
+        missionarios_esquerda, canibais_esquerda, barco, missionarios_direita, canibais_direita = state
+
+        # Verifica se o estado é válido
+        if missionarios_esquerda < 0 or canibais_esquerda < 0 or missionarios_direita < 0 or canibais_direita < 0:
+            return False
+        if missionarios_esquerda > 3 or canibais_esquerda > 3 or missionarios_direita > 3 or canibais_direita > 3:
+            return False
+        if (missionarios_esquerda < canibais_esquerda and missionarios_esquerda > 0) or (missionarios_direita < canibais_direita and missionarios_direita > 0):
+            print("ERRO: Há mais canibais que missionários em uma das bordas!\n")
+            return False
+        print(">>>Estado aceito.<<<\n")
+        return True
+
+    def generate_next_states(self, state):
+        states = []
+        missionarios_esquerda, canibais_esquerda, barco, missionarios_direita, canibais_direita = state
+
+        if barco == 'esquerda':
+            for m in range(3):
+                for c in range(3):
+                    if m + c > 0 and m + c <= 2:
+                        new_state = (
+                            missionarios_esquerda - m,
+                            canibais_esquerda - c,
+                            'direita',
+                            missionarios_direita + m,
+                            canibais_direita + c
+                        )
+                        print(f"Novo estado: {new_state}")
+                        if self.is_valid_state(new_state):
+                            states.append(new_state)
+        else:
+            for m in range(3):
+                for c in range(3):
+                    if m + c > 0 and m + c <= 2:
+                        new_state = (
+                            missionarios_esquerda + m,
+                            canibais_esquerda + c,
+                            'esquerda',
+                            missionarios_direita - m,
+                            canibais_direita - c
+                        )
+                        print(f"Novo estado: {new_state}")
+                        if self.is_valid_state(new_state):
+                            states.append(new_state)
+
+        return states
+
+    def heuristic(self, state):
+        missionarios_esquerda, canibais_esquerda, _, _, _ = state
+        return missionarios_esquerda + canibais_esquerda
+
+    def greedy_search(self, state, path, depth):
+        self.estados_visitados.add(state)
+        print(f"\n---------Estado atual: {state} (Profundidade: {depth})------------")
+        if state == self.goal_state:
+            return path
+
+        next_states = self.generate_next_states(state)
+        next_states.sort(key=self.heuristic)  # Ordena os próximos estados com base na heurística
+        for next_state in next_states:
+            print(f"\nVerificando próximo estado: {next_state}")
+            if next_state not in self.estados_visitados:
+                print("Estado não visitado!!")
+                new_path = path + [next_state]
+                result = self.greedy_search(next_state, new_path, depth+1)
+                if result:
+                    return result
+            else:
+                print("Estado já visitado!")
+        return None
+
+    def resolver(self):
+        return self.greedy_search(self.start_state, [self.start_state], 0)
+    
+class BuscaAStar:
+    def __init__(self, start_state, goal_state):
+        self.estados_visitados = set()
+        self.start_state = start_state
+        self.goal_state = goal_state
+
+    def is_valid_state(self, state):
+        missionarios_esquerda, canibais_esquerda, barco, missionarios_direita, canibais_direita = state
+
+        # Verifica se o estado é válido
+        if missionarios_esquerda < 0 or canibais_esquerda < 0 or missionarios_direita < 0 or canibais_direita < 0:
+            return False
+        if missionarios_esquerda > 3 or canibais_esquerda > 3 or missionarios_direita > 3 or canibais_direita > 3:
+            return False
+        if (missionarios_esquerda < canibais_esquerda and missionarios_esquerda > 0) or (missionarios_direita < canibais_direita and missionarios_direita > 0):
+            print("ERRO: Há mais canibais que missionários em uma das bordas!\n")
+            return False
+        print(">>>Estado aceito.<<<\n")
+        return True
+
+    def generate_next_states(self, state):
+        states = []
+        missionarios_esquerda, canibais_esquerda, barco, missionarios_direita, canibais_direita = state
+
+        if barco == 'esquerda':
+            for m in range(3):
+                for c in range(3):
+                    if m + c > 0 and m + c <= 2:
+                        new_state = (
+                            missionarios_esquerda - m,
+                            canibais_esquerda - c,
+                            'direita',
+                            missionarios_direita + m,
+                            canibais_direita + c
+                        )
+                        print(f"Novo estado: {new_state}")
+                        if self.is_valid_state(new_state):
+                            states.append(new_state)
+        else:
+            for m in range(3):
+                for c in range(3):
+                    if m + c > 0 and m + c <= 2:
+                        new_state = (
+                            missionarios_esquerda + m,
+                            canibais_esquerda + c,
+                            'esquerda',
+                            missionarios_direita - m,
+                            canibais_direita - c
+                        )
+                        print(f"Novo estado: {new_state}")
+                        if self.is_valid_state(new_state):
+                            states.append(new_state)
+        return states
+
+    def heuristic(self, state):
+        missionarios_esquerda, canibais_esquerda, _, _, _ = state
+        return abs(missionarios_esquerda - 0) + abs(canibais_esquerda - 0)
+
+    def a_star_search(self, state, path, depth):
+        self.estados_visitados.add(state)
+        print(f"\n---------Estado atual: {state} (Profundidade: {depth})------------")
+        if state == self.goal_state:
+            return path
+
+        next_states = self.generate_next_states(state)
+        next_states.sort(key=lambda x: self.heuristic(x) + len(path))  # Ordena os próximos estados com base na função de custo combinada
+        for next_state in next_states:
+            print(f"\nVerificando próximo estado: {next_state}")
+            if next_state not in self.estados_visitados:
+                print("Estado não visitado!!")
+                new_path = path + [next_state]
+                result = self.a_star_search(next_state, new_path, depth + 1)  # Incrementa a profundidade em cada chamada recursiva
+                if result:
+                    return result
+            else:
+                print("Estado já visitado!")
+        return None
+
+
+    def resolver(self):
+        return self.a_star_search(self.start_state, [self.start_state], 0)
---
+++ missionarios e canibais/main.py
@@ -0,0 +1,88 @@
+from canibais import BuscaProfundidade, BuscaGulosa, BuscaAStar
+import time
+
+def main():
+    quantidade = 3    
+    canibais, missionarios = quantidade, quantidade
+        
+    while True:
+        start_state = (missionarios, canibais, 'esquerda', 0, 0)
+        goal_state = (0, 0, 'direita', missionarios, canibais)
+
+        print_menu()
+        option = get_menu_choice()
+
+        t_start, t_end = 0, 0
+
+        if option == 0:
+            print("Saindo do programa...")
+            break
+
+        elif option == 1:
+            # Executar busca em largura
+            # ...
+            pass
+
+        elif option == 2:
+            # Executar busca profundidade
+            # ...
+            busca = BuscaProfundidade(start_state, goal_state)
+            t_start = time.perf_counter()
+            solucao = busca.resolver()
+            t_end = time.perf_counter()
+            printSolucao(solucao, t_end, t_start)
+
+        elif option == 3:
+            # Executar busca gulosa
+            # ...
+            busca = BuscaGulosa(start_state, goal_state)
+            t_start = time.perf_counter()
+            solucao = busca.resolver()
+            t_end = time.perf_counter()
+            printSolucao(solucao, t_end, t_start)
+
+        elif option == 4:
+            # Executar busca A*
+            # ...
+            busca = BuscaAStar(start_state, goal_state)
+            t_start = time.perf_counter()
+            solucao = busca.resolver()
+            t_end = time.perf_counter()
+            printSolucao(solucao, t_end, t_start)
+
+
+def print_menu():
+    print('============= MENU =============')
+    print('1 - Busca em Largura')
+    print('2 - Busca em Profundidade')
+    print('3 - Busca Gulosa*')
+    print('4 - Busca A*')
+    print('0 - Sair')
+    print('================================')
+
+
+def get_menu_choice():
+    while True:
+        try:
+            choice = int(input("Digite o número da opção desejada: "))
+            if choice in [0, 1, 2, 3, 4]:
+                return choice
+            else:
+                print("Opção inválida. Tente novamente.")
+        except ValueError:
+            print("Entrada inválida. Digite um número.")
+
+
+def printSolucao(solucao, t_end, t_start):
+    if solucao:
+        print("Solução encontrada:")
+        for state in solucao:
+            print(state)
+    else:
+        print('>>>>>>>>>> Não foi encontrada uma solução. <<<<<<<<<<<<<<')
+    
+    print('\nTempo decorrido: {:.4f} ms\n\n'.format(t_end - t_start))
+
+
+if __name__ == "__main__":
+    main()
