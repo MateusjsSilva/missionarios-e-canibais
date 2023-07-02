22d40980300a87fec3dcf4b600783d955539c270
João Filipe <joaofilipeb045@gmail.com>
2023-07-02 09:00:17 -0300

    Add the tree plot

--- missionarios e canibais/main.py
+++ missionarios e canibais/main.py
@@ -1,10 +1,38 @@
 from canibais import BuscaProfundidade, BuscaLargura, BuscaGulosa, BuscaAStar
 import time
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def criar_arvore(busca):
+    grafo = nx.DiGraph()
+    for estado in busca.estados_visitados:
+        grafo.add_node(estado)
+
+    for estado in busca.estados_visitados:
+        next_states = busca.generate_next_states(estado)
+        for next_state in next_states:
+            if next_state in busca.estados_visitados:
+                grafo.add_edge(estado, next_state)
+
+    plotar_arvore_estados(grafo)
+
+
+def plotar_arvore_estados(grafo):
+    pos = nx.spring_layout(grafo)
+    nx.draw_networkx(grafo, pos, with_labels=True, font_size=9, font_family="Comic Sans MS", node_size=1000,
+                     node_color="lightblue", edge_color="gray", arrows=True, arrowstyle='->, head_width=0.3, head_length=1.0')
+
+    plt.title("Árvore de Estados")
+    plt.axis("off")
+
+    plt.show()
+
 
 def main():
-    quantidade = 3    
+    quantidade = 3
     canibais, missionarios = quantidade, quantidade
-        
+
     while True:
         start_state = (missionarios, canibais, 'esquerda', 0, 0)
         goal_state = (0, 0, 'direita', missionarios, canibais)
@@ -25,6 +53,7 @@ def main():
             solucao = busca.resolver()
             t_end = time.perf_counter()
             printSolucao(solucao, t_end, t_start)
+            criar_arvore(busca)
 
         elif option == 2:
             # Executar busca profundidade
@@ -34,6 +63,7 @@ def main():
             solucao = busca.resolver()
             t_end = time.perf_counter()
             printSolucao(solucao, t_end, t_start)
+            criar_arvore(busca)
 
         elif option == 3:
             # Executar busca gulosa
@@ -43,6 +73,7 @@ def main():
             solucao = busca.resolver()
             t_end = time.perf_counter()
             printSolucao(solucao, t_end, t_start)
+            criar_arvore(busca)
 
         elif option == 4:
             # Executar busca A*
@@ -52,6 +83,7 @@ def main():
             solucao = busca.resolver()
             t_end = time.perf_counter()
             printSolucao(solucao, t_end, t_start)
+            criar_arvore(busca)
 
 
 def print_menu():
@@ -83,8 +115,9 @@ def printSolucao(solucao, t_end, t_start):
             print(state)
     else:
         print('>>>>>>>>>> Não foi encontrada uma solução. <<<<<<<<<<<<<<')
-    
+
     print('\nTempo decorrido: {:.4f} ms\n\n'.format(t_end - t_start))
+
 
 if __name__ == "__main__":
     main()
